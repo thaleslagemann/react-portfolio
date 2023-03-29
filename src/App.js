45acc05f6fd43ac1e4abd2f14b0d9c0ee49ade96
Thales Lagemann <thalesalaugustol@gmail.com>
2023-03-29 13:14:54 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import photo_5 from './assets/photo_5_crop_nobg.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="container">
+          <div class="row">
+            <div class="header-bar">
+              
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-4 text-center">
+              <img src={photo_5} class="image-self" alt="me"></img>
+            </div>
+            <div class="col-8 text-center">
+              <h1 class="title-self">Thales Lagemann</h1>
+              <h4 class="subtitle-self">Bacharelado em Sistemas de Informação</h4>
+              <h4 class="subtitle-self">Universidade Federal de Santa Maria</h4>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
